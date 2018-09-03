096c71abfc77e71c7726d5e11211a8d79ca11cf7
Patrick Catthoor <patrickcatthoor@mac.com>
2018-09-03 10:12:21 +0000

    Added file field for image upload to profile

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180902201828) do
+ActiveRecord::Schema.define(version: 20180902090929) do
 
   create_table "contacts", force: :cascade do |t|
     t.string   "name"
@@ -35,12 +35,8 @@ ActiveRecord::Schema.define(version: 20180902201828) do
     t.string   "phone_number"
     t.string   "contact_email"
     t.text     "description"
-    t.datetime "created_at",          null: false
-    t.datetime "updated_at",          null: false
-    t.string   "avatar_file_name"
-    t.string   "avatar_content_type"
-    t.integer  "avatar_file_size"
-    t.datetime "avatar_updated_at"
+    t.datetime "created_at",    null: false
+    t.datetime "updated_at",    null: false
   end
 
   create_table "users", force: :cascade do |t|
